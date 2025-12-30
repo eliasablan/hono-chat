13af4f4928c1ab963f6b46b40f03e5c6d6614a5f
Elias Ablan <eliasablan93@gmail.com>
2025-12-30 12:22:41 -0400

    style(ui): update layout theme and add badge component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-muted antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-accent antialiased`}
       >
         <Providers>
           {children}
